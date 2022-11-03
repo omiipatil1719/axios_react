5b4eaf37ef2aed5f172bf63337ef6247298e5895
omiipatil <patilonk97@gmail.com>
2022-11-03 12:56:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import axios from "axios"
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function App() {
+
+const App = () => {
+  const [news, setnews] = useState([])
+
+  function fetchnews() {
+    axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=ba3637ea1dbd4836b55c97b10309e5c7")
+      .then((resp) => {
+        console.log(resp);
+        setnews(resp.data.articles)
+      })
+    console.log("clicked")
+
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Today news</h1>
+
+      <Button  onClick={fetchnews} style={{marginLeft:"100px", marginBottom:"50px"}}>Click for News</Button>{' '}
+      <div className='newsflex'>
+
+      {
+        news.map((value) => {
+          return (
+            <div className='col-4'>
+              <Card style={{width:"300px",height:"520px",margin:'10px'}}>
+                <Card.Img variant="top" src={value.urlToImage}  />
+
+                <Card.Body>
+                  <Card.Title>{value.title}</Card.Title>
+                  <Card.Text>
+                    {value.description  }
+                  
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </div>
+
+          )
+
+
+        })
+
+      }
+      </div>
+
+
+    </>)
 }
 
-export default App;
+export default App
